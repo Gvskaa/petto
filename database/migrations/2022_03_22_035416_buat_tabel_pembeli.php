ad5b954e3a5a308c2933514b83aafadd5dadaa76
Arbim:) <70119476+EzGame099@users.noreply.github.com>
2022-03-22 11:07:10 +0700

    Membuat tabel pembeli

---
+++ database/migrations/2022_03_22_035416_buat_tabel_pembeli.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class BuatTabelPembeli extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembeli', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_admin');
+            $table->string('nama_pembeli');
+            $table->string('email_pembeli');
+            $table->string('password_pembeli');
+            $table->string('jk_pembeli');
+            $table->string('tlp_pembeli');
+            $table->string('alamat_pembeli');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembeli');
+    }
+}
